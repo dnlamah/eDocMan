ef4b6178721ff8f49102f767f10eac3446c25e8a
DanielAmah <amahdanieljack@gmail.com>
2017-07-08 14:33:54 +0100

    add config

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: (app.get('env') === 'development') ? err : {}
+    error: (app.get('env') === 'production') ? err : {}
   });
 });
 
